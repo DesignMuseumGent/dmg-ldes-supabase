4a9d865b316412dadd85a13b14d7309f63f45491
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-02-21 08:26:49 +0100

    add cron for daily harvest.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import {fetchLDES, fetchLDES_OSLO} from "./utils/ldes_harvester.js";
 import * as cron from 'node-cron'
 
-cron.schedule('00 10 * * *', start); // run harvest every day at 10:00
+cron.schedule('09 00 * * *', start); // run harvest every day at 10:00
 async function start(){
     // insert CIDOC
     fetchLDES();
