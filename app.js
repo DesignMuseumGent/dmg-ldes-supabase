7583dec1d8d6d298207befed76b7e8b4c0c14692
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-02-24 12:22:46 +0100

    close #3 add harvester for thesaurus.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-import {fetchLDES, fetchLDES_OSLO} from "./utils/ldes_harvester.js";
+import {fetchObjectLDES, fetchObjectLDES_OSLO, fetchThesaurusLDES} from "./utils/ldes_harvester.js";
 import * as cron from 'node-cron'
 
 cron.schedule('09 00 * * *', start); // run harvest every day at 10:00
 async function start(){
+    // OBJECTS
     // insert CIDOC
-    fetchLDES();
+    fetchObjectLDES();
     // insert OSLO
-    fetchLDES_OSLO();
+    fetchObjectLDES_OSLO();
+
+    //THESAURUS
+    fetchThesaurusLDES();
 }
 
-//console.log(dmg_objects_LDES);
