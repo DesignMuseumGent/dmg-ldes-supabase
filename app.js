55974be89d7a65879affb9df19f6dbe506684973
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-07-06 14:37:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,23 +4,42 @@ import {fetchObjectLDES,
     fetchPersonenLDES, fetchExhibitionLDES} from "./utils/ldes_harvester.js";
 import * as cron from 'node-cron'
 
-cron.schedule('33 14 * * *', start); // run harvest every day at 10:00
+console.log("---- harvester online -----")
+console.log("---- next harvest today at: 15:00 -----")
+
+
+cron.schedule('00 15 * * *', start); // run harvest every day at 10:00
 async function start(){
 
     // OBJECTS
     // insert CIDOC
+    console.log("-----------------------------")
+    console.log("start harvesting objects")
     fetchObjectLDES();
+    console.log("done harvesting objects")
+    
     // insert OSLO (UNSTABLE)
-    fetchObjectLDES_OSLO();
+    //fetchObjectLDES_OSLO();
 
     //THESAURUS
+    console.log("-----------------------------")
+    console.log("start harvesting thesaurus")
     fetchThesaurusLDES();
+    console.log("done harvesting thesaurus")
+
 
     // PERSONEN EN INSTELLINGEN
-    fetchPersonenLDES();
+    //fetchPersonenLDES();
 
     // EXHIBITION
+    console.log("-----------------------------")
+    console.log("start harvesting exhibitions")
     fetchExhibitionLDES();
+    console.log("done harvesting exhibitions")
+
+    console.log("-----------------------------")
+    console.log("---- next harvest today at: 15:00 -----")
+
 }
 
 //fetchObjectLDES();
